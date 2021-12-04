4bad56bfddc279a5fb70fe91ff4d5bb70a4fed53
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-04 14:34:56 -0500

    updating message route

--- controllers/api/message-routes.js
+++ controllers/api/message-routes.js
@@ -1,33 +1,50 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const { User, Pet, Message } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Find all comments
+// Find all messages
 router.get('/', async (req, res) => {
     try {
-        const commentData = await Comment.findAll({
-            include: [{ model: User, exclude: ['password'] }, { model: Post }],
+        const messageData = await Message.findAll({
+            include: [{ model: User, exclude: ['password'] }, { model: Pet }],
         });
-        res.status(200).json(commentData);
+        res.status(200).json(messageData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// Create new comment
-router.post('/', withAuth, async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const postData = await Comment.create({
-            comment_text: req.body.text,
-            postId: req.body.postId,
+        const messageData = await Message.findByPk(req.params.id, {
+            include: [{ model: User },{ model: Pet }]
+        });
+
+        if (!messageData) {
+            res.status(404).json({ message: 'No message found with this id!' });
+            return;
+        }
+
+        res.status(200).json(messageData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Create new message
+router.post('/', async (req, res) => {
+    try {
+        const messageData = await Message.create({
+            message_text: req.body.text,
+            petId: req.body.petId,
             userId: req.session.userId,
-            username: req.session.username,
+            username: req.session.firstName,
         });
 
         req.session.save(() => {
             req.session.loggedIn = true;
 
-            res.status(200).json(postData);
+            res.status(200).json(messageData);
         });
     } catch (err) {
         console.log(err);
