36a7edfaa30221eb8e68b6cd9a95d1aae0a8b9d8
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-12-08 15:02:16 -0500

    changed view of cards added another pet in seeds

--- seeds/messageData.js
+++ seeds/messageData.js
@@ -2,31 +2,31 @@ const { Message } = require('../models');
 
 const messageData = [
 	{
-		"message_text": "message goes here",
+		"message_text": "So cute can't wait to bring him home",
 		"petId": 1,
 		"userId": 1,
 		"username": "Hernandez",
 	},
 	{
-		"message_text": "message goes here",
+		"message_text": "Very adorable",
 		"petId": 2,
 		"userId": 2,
 		"username": "Smith",
 	},
 	{
-		"message_text": "message goes here",
+		"message_text": "Glad to find a home for her",
 		"petId": 3,
 		"userId": 3,
 		"username": "Rogers",
 	},
 	{
-		"message_text": "message goes here",
+		"message_text": "Happy to add a new member to the family",
 		"petId": 4,
 		"userId": 4,
 		"username": "Harrison",
 	},	
   {
-		"message_text": "message goes here",
+		"message_text": "Can't wait to meet him",
 		"petId": 5,
 		"userId": 5,
 		"username": "Garrison",
--- seeds/petData.js
+++ seeds/petData.js
@@ -5,7 +5,7 @@ const petData = [
 		"petName": "Fluffy",
 		"breed": "Yorkie",
 		"petType": "Dog",
-		"description": "small energetic puppy",
+		"description": "Small energetic puppy",
 		"age": 1,
 		"picture": "one",
 		"userId": 1
@@ -13,58 +13,68 @@ const petData = [
 	},
 	{
 		"petName": "Arthur",
-		"breed": "domestic short hair",
-		"petType": "cat",
-		"description": "white and black indoor adult cat",
+		"breed": "Domestic short-hair",
+		"petType": "Cat",
+		"description": "White and black indoor adult cat",
 		"age": 2,
 		"picture": "two",
 		"userId": 2
 	},
 	{
 		"petName": "Gwen",
-		"breed": "domestic short hair",
-		"petType": "cat",
-		"description": "black and white indoor adult cat",
+		"breed": "Domestic short-hair",
+		"petType": "Cat",
+		"description": "Black and white indoor adult cat",
 		"age": 3,
 		"picture": "three",
 		"userId": 3
 	},
 	{
 		"petName": "Blue",
-		"breed": "pitbull",
-		"petType": "dog",
-		"description": "young energetic pitbull",
+		"breed": "Blue Nose Pitbull",
+		"petType": "Dog",
+		"description": "Young playful pitbull",
 		"age": 1,
 		"picture": "four",
 		"userId": 1
 	},
 	{
 		"petName": "Winston",
-		"breed": "english bulldog",
-		"petType": "dog",
-		"description": "adult english bulldog well trained",
+		"breed": "English Bulldog",
+		"petType": "Dog",
+		"description": "Adult english bulldog well-trained",
 		"age": 5,
 		"picture": "five",
 		"userId": 2
 	},
 	{
 		"petName": "Duke",
-		"breed": "great dane",
-		"petType": "dog",
-		"description": "large loveable great dane puppy",
+		"breed": "Great Dane",
+		"petType": "Dog",
+		"description": "Large loveable great dane puppy",
 		"age": 1,
 		"picture": "six",
 		"userId": 3
 	},
 	{
-		"petName": "Grumpy",
-		"breed": "mixed",
-		"petType": "cat",
-		"description": "long hair indoor outdoor cat",
+		"petName": "Sammi",
+		"breed": "Mixed",
+		"petType": "Cat",
+		"description": "Short-hair indoor/outdoor cat",
 		"age": 3,
 		"picture": "seven",
 		"userId": 1
+	},
+	{
+		"petName": "Roby",
+		"breed": "Parakeet",
+		"petType": "Bird",
+		"description": "Beautiful green parakeet",
+		"age": 3,
+		"picture": "eight",
+		"userId": 2
 	}
+
 ];
 
 const seedPet = () => Pet.bulkCreate(petData);
